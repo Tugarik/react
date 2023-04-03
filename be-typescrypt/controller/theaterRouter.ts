2883392b197017634cab9146bb5f6c9f14d598a2
Tugarik <tugarik@gmail.com>
2023-04-03 13:09:46 +0800

    blabla

--- be-typescrypt/controller/theaterRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getTheatres } from "../services/theater-service";
-
-const theaterRouter = express.Router();
-
-theaterRouter.get("/theater", getTheatres);
-
-export default theaterRouter;
